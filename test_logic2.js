fdf8705f2807f07547d03177eccaea240d03f204
Diego Ledesma <diego@finbook.app>
2026-04-09 22:41:35 -0300

    chore: project cleanup and final coherence for public release

--- test_logic2.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-const os = require('os');
-const dbPath = path.join(os.homedir(), '.config', 'finbook', 'finanzas.db');
-const db = new sqlite3.Database(dbPath);
-
-console.log("Date offset test", new Date('2026-04-01').getMonth());
